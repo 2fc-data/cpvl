9c229cc14cacc9f5aa42a4084e7692c843e17e2b
Fernando <camargo.fernando@gmail.com>
2025-05-21 13:43:58 -0300

    home styles

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,41 @@
+import { animate, motion, useMotionTemplate, useMotionValue } from "framer-motion";
 import { HomeWrap } from "./Home.styles"
+import { useEffect } from "react";
+
+const COLORS = ["rgba(224, 226, 68, 0.5)", "rgba(71, 210, 245, 0.5)", "rgba(142, 68, 226, 0.5)", "rgba(245, 112, 71, 0.5)"];
 
 export const Home = () => {
+
+  const color = useMotionValue(COLORS[0]);
+
+  const backgroundImage = useMotionTemplate`radial-gradient
+  (125% 125% at 50% 0%, rgba(194, 214, 219, 0.5), ${color})`;
+
+  useEffect(() => {
+    animate(color, COLORS, {
+      ease: 'easeInOut',
+      duration: 10,
+      repeat: Infinity,
+      repeatType: 'reverse'
+    })
+  }, [])
+
   return (
     <HomeWrap>
-      <div className='home-content'>
-        <div className="home-title">
-          Clube Poços-caldense de Vôo Livre
+      <motion.section
+        style={{ backgroundImage }}
+      >
+        <div className='home-content'>
+          <div className="home-title">
+            Clube Poços-caldense de Vôo Livre
+          </div>
+          <div className="home-subtitle">
+            desde 1995
+          </div>
         </div>
-        <div className="home-subtitle">
-          desde 1995
-        </div>
-      </div>
+      </motion.section>
+
+
     </HomeWrap>
   )
 }
