b1e62b510ef49b75e8f2b2d70f8c293d5dc45bc5
Fernando <camargo.fernando@gmail.com>
2025-06-13 20:14:51 -0300

    Start  new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "normalize.css";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme/theme";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { createBrowserRouter, RouterProvider, Routes, Route } from "react-router-dom";
 import { GlobalStyles } from "./styles/global/globalStyles";
+
 import { BaseLayout } from "./components/layout/";
 import { About } from "./pages/about";
 import { Airspace } from './pages/airSpace';
@@ -13,26 +15,29 @@ import { Regiment } from "./pages/regiment";
 import { Signup } from './pages/signup';
 import { Statute } from "./pages/statute";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <BaseLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "airspace", element: <Airspace /> },
+      { path: "direction", element: <Direction /> },
+      { path: "home", element: <Home /> },
+      { path: "login", element: <Login /> },
+      { path: "regiment", element: <Regiment /> },
+      { path: "signup", element: <Signup /> },
+      { path: "statute", element: <Statute /> },
+    ],
+  },
+]);
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Router>
-        <GlobalStyles />
-        <Routes>
-          <Route path="/" element={<BaseLayout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="airspace" element={<Airspace />} />
-            <Route path="direction" element={<Direction />} />
-            <Route path="home" element={<Home />} />
-            <Route path="login" element={<Login />} />            
-            <Route path="regiment" element={<Regiment />} />
-            <Route path="signup" element={<Signup />} />
-            <Route path="statute" element={<Statute />} />
-            <Route path="*" element={<Home />} />
-          </Route>
-        </Routes>
-      </Router>
+      <GlobalStyles />
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
